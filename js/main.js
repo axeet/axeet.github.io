a743a0d2fe0496a440eb4435b968655d1736d648
souovan <souovan@protonmail.com>
2022-02-11 18:04:58 -0300

    Adding colors to template

--- js/main.js
+++ js/main.js
@@ -12,6 +12,28 @@ function get_input_value (){
 
     console.log(obj.name);
     console.log(obj.palette);
+
+
+switch (obj.palette) {
+    case fedora:
+        var cor = "fedora";
+        break;
+    case ubuntu:
+        var cor = "ubuntu";
+        break;
+    case redhat:
+        var cor = "redhat";
+        break;
+    case dracula:
+        var cor = "dracula";
+        break;
+    default:
+        var cor = "default";
+        break;
+}
+
+
+
     console.log(obj.title);
     console.log(" ");
 
@@ -27,8 +49,41 @@ function get_input_value (){
   "name": "Cheatsheet do vini",
   "palette": "default",
   "title": "RHCSA",
-  "systemd": "Configuração e controle de sistemas Linux",
-  "man systemd.unit": "Para mais informações",
-  "systemctl list-units": "Mostra todas unidades ativas e carregadas"
+  "cheat": [ {
+            "systemd": "Configuração e controle de sistemas Linux",
+            "man systemd.unit": "Para mais informações",
+            "systemctl list-units": "Mostra todas unidades ativas e carregadas"
+  }]
 } 
 */
+
+/* {
+    "titulo": "Axeet",
+    "palette": "default",
+    "autor": "mich",
+    "cheatsheet": "RedHat",
+    "topic": [
+        {
+        "topicName": "Manipulando Openshift",
+        "cheat": [
+            {
+                "codigoCheat": "oc get ns",
+                "descricaoCheat": "pegando o name space openshift"
+            },
+            {
+                "codigoCheat": "oc select project [nome do projeto]",
+                "descricaoCheat" : "entra dentro de umprojeto no openshift"
+            }
+        ]
+        }
+    ]
+} */
+
+/* obj.title = Axeet
+
+obj.cheat.codigoCheat[0] = oc get ns
+
+obj.cheat.descricaoCheat[0] = pegando o name space openshift */
+
+
+
