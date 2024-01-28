1610ea6a063df9ecb39d265283adf72a246e7f6f
michael.oliveira <michael.lopes@extreme.digital>
2024-01-28 15:14:46 -0300

    🧩 feat: colocando o editor do codemirror puro no axeet.
    Foi realizado a reorganização dos documentos e libs externas no projeto.
    
    versão do codeMirror: 5.65.16

---
+++ js/editor.js
@@ -0,0 +1,6 @@
+var editor = CodeMirror.fromTextArea(document.getElementById("axeetScript"), {
+    lineNumbers: true,
+    styleActiveLine: true,
+    matchBrackets: true,
+    theme: "dracula"
+});
