b79229ea8ba1b78b8e60370ba995d780fe3c5cd0
michael.oliveira <michael.lopes@extreme.digital>
2024-01-28 20:04:36 -0300

    :bug: fix: reajustando o tamanho do textarea para o codemirror.

--- js/editor.js
+++ js/editor.js
@@ -2,5 +2,27 @@ var editor = CodeMirror.fromTextArea(document.getElementById("axeetScript"), {
     lineNumbers: true,
     styleActiveLine: true,
     matchBrackets: true,
+    mode: "javascript",
     theme: "dracula"
 });
+
+var place =  '{\n' +
+    '"titleCheat": "Axeet",\n' +
+    '"theme": "axeet",\n' +
+    '"author": "Fulano",\n' +
+    '"cheatSheet": "Red Hat",\n' +
+    '"topic": {\n' +
+    '      "topicName": "Openshift",\n' +
+    '      "cheat": {\n' +
+    '            "codeCheat": "oc get ns",\n' +
+    '            "description": "pegando o name space"\n' +
+    '               },\n' +
+    '               {\n' +
+    '             "codeCheat": "oc select project [nome do projeto",\n' +
+    '             "entra dentro de um projeto no openshift"\n' +
+    '               }\n' +
+    '           }\n' +
+    '}';
+
+var width = window.innerWidth;
+editor.setSize(0.5*width, 0.5*width);
