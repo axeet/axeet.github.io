24f30bd056410d439e34d7d1a1c118fb2a67d9ca
michael.oliveira <michael.lopes@extreme.digital>
2024-02-23 12:24:44 -0300

    :bug: fix:  corrigido o bug onde criava um elemento null dentro do json

--- js/main.js
+++ js/main.js
@@ -42,23 +42,16 @@ function percorreTopicList(countTopic, axeet) {
 
 function percorreCheatList(countTopic, axeet) {
     console.log(countTopic)
-    if (countTopic > 1) {
         for (i = 0; countTopic > i; i++) {
-            for (var cheat in axeet.topic[countTopic - 1].cheat.value) {
-                axeet.topic[countTopic - 1].cheat[i + 1] = new Cheats("", "")
-            }
-            axeet.topic[i].cheat[i + 1] = new Cheats("", "")
-        }
-    } else {
-        for (i = 0; countTopic > i; i++) {
-            var countCheat = axeet.topic[i].cheat.length;
-            console.log(countCheat)
-            for (j = 0; j < countCheat; j++) {
-                console.log(axeet.topic[i].cheat[j + 1])
-                if (axeet.topic[i].cheat[j + 1] === undefined) {
-                    axeet.topic[i].cheat[j + 1] = new Cheats("", "")
+            if (axeet.topic[i + 1]=== undefined){
+                var countCheat = axeet.topic[i].cheat.length;
+                console.log(countCheat)
+                for (j = 0; j < countCheat; j++) {
+                    console.log(axeet.topic[i].cheat[j + 1])
+                    if (axeet.topic[i].cheat[j + 1] === undefined) {
+                        axeet.topic[i].cheat[j + 1] = new Cheats("", "")
+                    }
                 }
             }
         }
-    }
 }
